18cc99272a921f834ef7a5f6e25624507fae3a58
iTango21 <itango21@gmail.com>
2022-04-28 14:33:39 +0300

    создать Класс "App" реализовать ф-ю "closing"

--- main.py
+++ main.py
@@ -11,3 +11,28 @@ if __name__ == "__main__":
     print('start project')
     # app = App()
     # app.mainloop()
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Інформаційно-пошукова система \"Електронна фільмотека\"")
+        self.maxsize(1024, 700)
+        self.iconbitmap('oscar.ico')
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+        self.filter = "1"
+        # 1 - весь список
+        # 2 - за роком створення
+        # 3 - за країною
+        # 4 - за жанром
+        self.country_filter = "США"
+        self.year_filter = 1997
+        self.before_filter = True
+        self.genre_filter = 'драма'
+
+    # функція закриття головного вікна програми
+    def on_closing(self):
+        if mb.askokcancel("Вихід", "Ви дійсно бажаєте вийти?"):
+            self.curs.close()
+            self.conn.close()
+            self.destroy()

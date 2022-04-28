a79c86ed40162fd451a6e9d1ea6b98481dd2f35f
iTango21 <itango21@gmail.com>
2022-04-28 14:41:14 +0300

    создать класс "GenreDov", инициализировать и расставить геометрию

--- genredov.py
+++ genredov.py
@@ -24,3 +24,43 @@ def file_write():
         writer.writerow(
             genre
         )
+
+
+class GenreDov(tk.Toplevel):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.title("Довідник жанрів")
+        self.resizable(0, 0)
+
+        self.frame = tk.Frame(self, height=200, width=400, padx=5,
+                              pady=5)
+        self.frame2 = tk.Frame(self, height=50, width=400, padx=5,
+                               pady=5)
+
+        self.scrollbar = tk.Scrollbar(self.frame)
+
+        self.listbox = tk.Listbox(self.frame, height=10, width=30, selectmode=tk.SINGLE,
+                                  yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.listbox.yview)
+
+        for elem in genre:
+            self.listbox.insert(tk.END, elem)
+
+        self.add = tk.Button(self.frame2, text="  Додати  ", command=self.open_add)
+        self.edit = tk.Button(self.frame2, text="Редагувати", command=self.open_edit)
+        self.dell = tk.Button(self.frame2, text=" Видалити ", command=self.open_del)
+
+        self.frame.grid(row=0, column=0, padx=5, pady=5)
+        self.listbox.pack(side=tk.LEFT, fill=tk.BOTH)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.frame2.grid(row=0, column=1, padx=5, pady=5)
+
+        self.add.grid(row=0, column=0, padx=15, pady=10, ipadx=20)
+        self.edit.grid(row=1, column=0, padx=15, pady=10, ipadx=15)
+        self.dell.grid(row=2, column=0, padx=15, pady=10, ipadx=16)
+
+
+if __name__ == "__main__":
+    print("Запускайте файл main.py")

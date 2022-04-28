5f5a7da539d42cdcfdc40ac01cbf27e8b95d38ca
iTango21 <itango21@gmail.com>
2022-04-28 14:19:56 +0300

    создать основной файл проэкта

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+import tkinter.messagebox as mb
+import tkinter.simpledialog as sd
+import tkinter.ttk as ttk
+import os
+
+import pandas as pd
+
+
+if __name__ == "__main__":
+    print('start project')
+    # app = App()
+    # app.mainloop()
